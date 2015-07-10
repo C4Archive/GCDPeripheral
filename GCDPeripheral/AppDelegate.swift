5882b379df350206f49f5418232b3baca9cdc41b
-- C4 -- <info@c4ios.com>
2015-07-09 19:56:55 -0700

    Adds GCDAsyncSocket
    Sets up main app delegate to search for services

--- GCDPeripheral/AppDelegate.swift
+++ GCDPeripheral/AppDelegate.swift
@@ -9,38 +9,121 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+public class AppDelegate: UIResponder, UIApplicationDelegate, NSNetServiceBrowserDelegate, NSNetServiceDelegate, GCDAsyncSocketDelegate {
+    var netServiceBrowser : NSNetServiceBrowser?
+    var serverService : NSNetService?
+    var serverAddresses : [NSData]?
+    var asyncSocket : GCDAsyncSocket?
+    var connected = false
 
-    var window: UIWindow?
+    public var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+    public func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        netServiceBrowser = NSNetServiceBrowser()
+        netServiceBrowser?.delegate = self
+        netServiceBrowser?.searchForServicesOfType("_m-o._tcp.", inDomain: "local.")
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    public func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    public func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    public func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    public func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    public func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        println("applicationWillTerminate")
+        asyncSocket?.disconnect()
     }
 
+    public func netServiceBrowser(aNetServiceBrowser: NSNetServiceBrowser, didFindDomain domainString: String, moreComing: Bool) {
+        println("didFindDomain")
+    }
+
+    public func netServiceBrowser(aNetServiceBrowser: NSNetServiceBrowser, didFindService aNetService: NSNetService, moreComing: Bool) {
+        println("didFindService")
+
+        if (serverService == nil) {
+            serverService = aNetService;
+            serverService?.delegate = self
+            serverService?.resolveWithTimeout(5.0)
+        }
+    }
+
+    public func netServiceBrowser(aNetServiceBrowser: NSNetServiceBrowser, didNotSearch errorDict: [NSObject : AnyObject]) {
+        println("didNotSearch \(errorDict)")
+    }
+
+    public func netServiceBrowser(aNetServiceBrowser: NSNetServiceBrowser, didRemoveDomain domainString: String, moreComing: Bool) {
+        println("didRemoveDomain")
+    }
+
+    public func netServiceBrowser(aNetServiceBrowser: NSNetServiceBrowser, didRemoveService aNetService: NSNetService, moreComing: Bool) {
+        println("didRemoveService")
+    }
+
+    public func netServiceBrowserDidStopSearch(aNetServiceBrowser: NSNetServiceBrowser) {
+        println("didStopSearch")
+    }
+
+    public func netServiceBrowserWillSearch(aNetServiceBrowser: NSNetServiceBrowser) {
+        println("willSearch")
+    }
+
+    public func netServiceDidResolveAddress(sender: NSNetService) {
+        if serverAddresses == nil {
+            serverAddresses = [NSData]()
+            if let count = sender.addresses?.count,
+                let addresses = sender.addresses as? [NSData] {
+                    for i in 0..<count {
+                        serverAddresses?.append(addresses[i])
+                    }
+            }
+        }
+
+        if (asyncSocket == nil) {
+            asyncSocket = GCDAsyncSocket(delegate: self, delegateQueue: dispatch_get_main_queue())
+        }
+
+        connectToNextAddress()
+    }
+
+    public func connectToNextAddress() {
+        var done = false
+
+        while !done && serverAddresses?.count > 0 {
+            var address = serverAddresses?[0]
+            serverAddresses?.removeAtIndex(0)
+            var error : NSError?
+            if let response = asyncSocket?.connectToAddress(address , error: &error) {
+                done = response
+            }
+        }
+    }
+
+    public func socket(sock: GCDAsyncSocket!, didConnectToHost host: String!, port: UInt16) {
+        connected = true
+    }
+
+    public func socketDidDisconnect(sock: GCDAsyncSocket!, withError err: NSError!) {
+        if !connected {
+            connectToNextAddress()
+        }
+    }
 
 }
 
